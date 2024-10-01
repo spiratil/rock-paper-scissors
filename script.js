e495dea1ae5f7c4e10d340f427e214e9504cdf1e
David Whitman <125731633+spiratil@users.noreply.github.com>
2024-10-01 17:28:19 +0930

    Write logic for computer's choice

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log("Hello World!");
+// Compute the computer's choice
+let getComputerChoice = function() {
+  let rand = Math.ceil(Math.random() * 3);
+  if (rand == 1) return 'rock';
+  else if (rand == 2) return 'paper';
+  else return 'scissors';
+}
+
+console.log(getComputerChoice());
